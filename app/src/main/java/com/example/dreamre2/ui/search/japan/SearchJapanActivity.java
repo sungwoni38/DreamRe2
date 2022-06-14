14aee366881d4ee8910cb0c77930fda855ff2870
LG <hsw522hsw1221@gmail.com>
2022-06-15 04:55:25 +0900

    commitSW_3

--- app/src/main/java/com/example/dreamre2/ui/search/japan/SearchJapanActivity.java
+++ app/src/main/java/com/example/dreamre2/ui/search/japan/SearchJapanActivity.java
@@ -1,8 +1,12 @@
 package com.example.dreamre2.ui.search.japan;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.example.dreamre2.R;
 
@@ -12,5 +16,17 @@ public class SearchJapanActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_japan);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
+        ImageView img_before = findViewById(R.id.img_before);
+        img_before.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent01 = new Intent(); //인텐트 객체 생성
+                setResult(RESULT_OK, intent01);
+                finish();
+            }
+        });
     }
 }
