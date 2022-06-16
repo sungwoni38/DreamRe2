e373f095f6e930f10f9c10ce4be6f5c0548dd88c
dr4549 <89069633+dr4549@users.noreply.github.com>
2022-06-16 16:20:46 +0900

    commitJH_7

--- app/src/main/java/com/example/dreamre2/ui/search/SearchFragment.java
+++ app/src/main/java/com/example/dreamre2/ui/search/SearchFragment.java
@@ -14,6 +14,8 @@ import androidx.fragment.app.Fragment;
 import com.example.dreamre2.R;
 import com.example.dreamre2.databinding.FragmentSearchBinding;
 import com.example.dreamre2.ui.search.Italy.SearchItalyActivity;
+import com.example.dreamre2.ui.search.bunsik.SearchBunsikActivity;
+import com.example.dreamre2.ui.search.china.SearchChinaActivity;
 import com.example.dreamre2.ui.search.japan.SearchJapanActivity;
 import com.example.dreamre2.ui.search.korea.SearchKoreaActivity;
 
@@ -21,7 +23,7 @@ public class SearchFragment extends Fragment {
 
     private FragmentSearchBinding binding;
     private View view;
-    private CardView cardView_kor, cardView_italy, cardView_japan;
+    private CardView cardView_kor, cardView_italy, cardView_japan, cardView_China, cardView_bunsik;
 
     @Nullable
     @Override
@@ -57,6 +59,28 @@ public class SearchFragment extends Fragment {
                 startActivity(intent_japan);
             }
         });
+
+
+        cardView_China = (CardView) view.findViewById(R.id.cardView_chi);
+        cardView_China.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent_china = new Intent(getActivity(), SearchChinaActivity.class);
+                intent_china.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                startActivity(intent_china);
+            }
+        });
+
+        cardView_bunsik = (CardView) view.findViewById(R.id.cardView_bunsik);
+        cardView_bunsik.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent_bunsik = new Intent(getActivity(), SearchBunsikActivity.class);
+                intent_bunsik.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                startActivity(intent_bunsik);
+            }
+        });
+
         return view;
     }
 
